fa7fc913093b94674f081348409ffdc9d601cc10
makyomao <maoyingxia@huawei.com>
2016-04-26 20:26:58 +0800

    init.pp modification

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class puppetlabs_netdev_stdlib_ce{
 
 network_device{'CE-5850':
-name => 'CE-5850',
+name     => 'CE-5850',
 ipaddress => '10.136.68.123',
 username => 'rootDC',
 password => 'root_DC123',
