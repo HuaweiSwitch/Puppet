0c799860d8197e30c03bb322fe39f311b3e59a59
makyomao <maoyingxia@huawei.com>
2016-04-26 17:09:43 +0800

    Update netconf API.

--- lib/puppet/provider/ce/api/car/car_api.rb
+++ lib/puppet/provider/ce/api/car/car_api.rb
@@ -15,21 +15,21 @@ module Puppet
           car_array = []
           session = Puppet::NetDev::CE::Device.session
 
-          get_car_xml = '<rpc><get><filter type="subtree"><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfCarTmpls><qosIfCarTmpl><CarTmplName></CarTmplName><cir></cir><pir></pir><cbs></cbs><pbs></pbs></qosIfCarTmpl></qosIfCarTmpls></qos></filter></get></rpc>'
+          get_car_xml = '<rpc><get><filter type="subtree"><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfCarTmpls><qosIfCarTmpl><carTmplName></carTmplName><cir></cir><pir></pir><cbs></cbs><pbs></pbs></qosIfCarTmpl></qosIfCarTmpls></qos></filter></get></rpc>'
           car_all = session.rpc.do_config(get_car_xml)
 
           if car_all.element_children.count != 0
             car_element = car_all.first_element_child.first_element_child
             car_element.element_children.each do |car_elem|
               car_doc_speed = Nokogiri::XML(car_elem.to_s)
-              car_name = car_doc_speed.xpath('/qosIfCarTmpl/CarTmplName').text
+              car_name = car_doc_speed.xpath('/qosIfCarTmpl/carTmplName').text
               car_speed = car_doc_speed.xpath('/qosIfCarTmpl/cir').text
 
               property_hash = { ensure: :present }
               property_hash[:name] = car_name
               property_hash[:speed] = car_speed
 
-              get_car_apply_xml = '<rpc><get><filter type="subtree"><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfQoss><qosIfCars><qosIfCar><ifName></ifName><direction>inbound</direction><CarTmplName></CarTmplName></qosIfCar></qosIfCars></qosIfQoss></qos></filter></get></rpc>'
+              get_car_apply_xml = '<rpc><get><filter type="subtree"><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfQoss><qosIfCars><qosIfCar><ifName></ifName><direction>inbound</direction><carTmplName></carTmplName></qosIfCar></qosIfCars></qosIfQoss></qos></filter></get></rpc>'
 
               car_apply_all = session.rpc.do_config(get_car_apply_xml)
 
@@ -37,7 +37,7 @@ module Puppet
                 car_apply_elements = car_apply_all.first_element_child.first_element_child.first_element_child
                 car_apply_elements.element_children.each do |car_apply_elem|
                   car_doc = Nokogiri::XML(car_apply_elem.to_s)
-                  car_name_get = car_doc.xpath('/qosIfCar/CarTmplName').text
+                  car_name_get = car_doc.xpath('/qosIfCar/carTmplName').text
                   next unless car_name_get == car_name
 
                   car_interface = car_doc.xpath('/qosIfCar/ifName').text
@@ -54,23 +54,23 @@ module Puppet
 
         def set_car(resource)
           session = Puppet::NetDev::CE::Device.session
-          create_car_xml = '<rpc><edit-config><target><running/></target><default-operation>merge</default-operation><error-option>rollback-on-error</error-option><config><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfCarTmpls><qosIfCarTmpl operation="create"><CarTmplName>' + (resource[:name]).to_s + '</CarTmplName><cir>' + (resource[:speed]).to_s + '</cir><pir></pir><cbs></cbs><pbs></pbs></qosIfCarTmpl></qosIfCarTmpls></qos></config></edit-config></rpc>'
+          create_car_xml = '<rpc><edit-config><target><running/></target><default-operation>merge</default-operation><error-option>rollback-on-error</error-option><config><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfCarTmpls><qosIfCarTmpl operation="create"><carTmplName>' + (resource[:name]).to_s + '</carTmplName><cir>' + (resource[:speed]).to_s + '</cir><pir></pir><cbs></cbs><pbs></pbs></qosIfCarTmpl></qosIfCarTmpls></qos></config></edit-config></rpc>'
           session.rpc.do_config(create_car_xml)
 
           if !resource[:interface_name].nil? && !resource[:interface_name].empty?
-            set_car_apply_xml = '<rpc><edit-config><target><running/></target><default-operation>merge</default-operation><error-option>rollback-on-error</error-option><config><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfQoss><qosIfCars><qosIfCar operation="create"><ifName>' + (resource[:interface_name]).to_s + '</ifName><direction>inbound</direction><CarTmplName>' + (resource[:name]).to_s + '</CarTmplName></qosIfCar></qosIfCars></qosIfQoss></qos></config></edit-config></rpc>'
+            set_car_apply_xml = '<rpc><edit-config><target><running/></target><default-operation>merge</default-operation><error-option>rollback-on-error</error-option><config><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfQoss><qosIfCars><qosIfCar operation="create"><ifName>' + (resource[:interface_name]).to_s + '</ifName><direction>inbound</direction><carTmplName>' + (resource[:name]).to_s + '</carTmplName></qosIfCar></qosIfCars></qosIfQoss></qos></config></edit-config></rpc>'
             session.rpc.do_config(set_car_apply_xml)
           end
 
           if !resource[:interface_name].nil? && resource[:interface_name].empty?
             car_interface = nil
-            get_car_apply_xml = '<rpc><get><filter type="subtree"><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfQoss><qosIfCars><qosIfCar><ifName></ifName><direction>inbound</direction><CarTmplName></CarTmplName></qosIfCar></qosIfCars></qosIfQoss></qos></filter></get></rpc>'
+            get_car_apply_xml = '<rpc><get><filter type="subtree"><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfQoss><qosIfCars><qosIfCar><ifName></ifName><direction>inbound</direction><carTmplName></carTmplName></qosIfCar></qosIfCars></qosIfQoss></qos></filter></get></rpc>'
             car_apply_all = session.rpc.do_config(get_car_apply_xml)
             if car_apply_all.element_children.count != 0
               car_apply_elements = car_apply_all.first_element_child.first_element_child.first_element_child
               car_apply_elements.element_children.each do |car_apply_elem|
                 car_doc = Nokogiri::XML(car_apply_elem.to_s)
-                car_name_get = car_doc.xpath('/qosIfCar/CarTmplName').text
+                car_name_get = car_doc.xpath('/qosIfCar/carTmplName').text
                 if car_name_get == resource[:name]
                   car_interface = car_doc.xpath('/qosIfCar/ifName').text
              end
@@ -86,21 +86,21 @@ module Puppet
 
         def create_car(resource)
           session = Puppet::NetDev::CE::Device.session
-          create_car_xml = '<rpc><edit-config><target><running/></target><default-operation>merge</default-operation><error-option>rollback-on-error</error-option><config><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfCarTmpls><qosIfCarTmpl operation="create"><CarTmplName>' + (resource[:name]).to_s + '</CarTmplName><cir>' + (resource[:speed]).to_s + '</cir><pir></pir><cbs></cbs><pbs></pbs></qosIfCarTmpl></qosIfCarTmpls></qos></config></edit-config></rpc>'
+          create_car_xml = '<rpc><edit-config><target><running/></target><default-operation>merge</default-operation><error-option>rollback-on-error</error-option><config><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfCarTmpls><qosIfCarTmpl operation="create"><carTmplName>' + (resource[:name]).to_s + '</carTmplName><cir>' + (resource[:speed]).to_s + '</cir><pir></pir><cbs></cbs><pbs></pbs></qosIfCarTmpl></qosIfCarTmpls></qos></config></edit-config></rpc>'
           session.rpc.do_config(create_car_xml)
           end
 
         def delete_car(resource)
           session = Puppet::NetDev::CE::Device.session
           car_interface = nil
-          get_car_apply_xml = '<rpc><get><filter type="subtree"><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfQoss><qosIfCars><qosIfCar><ifName></ifName><direction>inbound</direction><CarTmplName></CarTmplName></qosIfCar></qosIfCars></qosIfQoss></qos></filter></get></rpc>'
+          get_car_apply_xml = '<rpc><get><filter type="subtree"><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfQoss><qosIfCars><qosIfCar><ifName></ifName><direction>inbound</direction><carTmplName></carTmplName></qosIfCar></qosIfCars></qosIfQoss></qos></filter></get></rpc>'
           car_apply_all = session.rpc.do_config(get_car_apply_xml)
 
           if car_apply_all.element_children.count != 0
             car_apply_elements = car_apply_all.first_element_child.first_element_child.first_element_child
             car_apply_elements.element_children.each do |car_apply_elem|
               car_doc = Nokogiri::XML(car_apply_elem.to_s)
-              car_name_get = car_doc.xpath('/qosIfCar/CarTmplName').text
+              car_name_get = car_doc.xpath('/qosIfCar/carTmplName').text
               if car_name_get == resource[:name]
                 car_interface = car_doc.xpath('/qosIfCar/ifName').text
                end
@@ -112,7 +112,7 @@ module Puppet
             session.rpc.do_config(delete_car_apply_xml)
           end
 
-          delete_car_xml = '<rpc><edit-config><target><running/></target><default-operation>merge</default-operation><error-option>rollback-on-error</error-option><config><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfCarTmpls><qosIfCarTmpl operation="delete"><CarTmplName>' + (resource[:name]).to_s + '</CarTmplName></qosIfCarTmpl></qosIfCarTmpls></qos></config></edit-config></rpc>'
+          delete_car_xml = '<rpc><edit-config><target><running/></target><default-operation>merge</default-operation><error-option>rollback-on-error</error-option><config><qos xmlns="http://www.huawei.com/netconf/vrp" content-version="1.0" format-version="1.0"><qosIfCarTmpls><qosIfCarTmpl operation="delete"><carTmplName>' + (resource[:name]).to_s + '</carTmplName></qosIfCarTmpl></qosIfCarTmpls></qos></config></edit-config></rpc>'
           session.rpc.do_config(delete_car_xml)
           end
       end
