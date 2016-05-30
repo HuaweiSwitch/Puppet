e9b978c0d34a48bd4c23642c1ee34ac8cba98cc9
makyomao <maoyingxia@huawei.com>
2016-05-30 17:20:47 +0800

    Add description of LICENSE and Modify
    Modify for singleton mode

--- lib/puppet/provider/ce/device_tel/device_tel.rb
+++ lib/puppet/provider/ce/device_tel/device_tel.rb
@@ -1,3 +1,15 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # encoding: utf-8
 require 'net/netconf'
 require 'net/telnet'
@@ -9,51 +21,45 @@ module Puppet
   module NetDev
     # CE namespace
     module CE
-      class Device
-        attr_accessor :telnet_session
-        attr_accessor :command_telnet_api
+      class Device_tel
+        private_class_method :new
 
         attr_accessor :telnet_hostip
         attr_accessor :telnet_username
         attr_accessor :telnet_password
-
-        # puts @@netconf_hostip
-        # create a session to connect to device
+        
+        @@telnet_session     = nil
+        @@tel_instance       = nil
+        @@command_telnet_api = nil
 
         def self.telnet_session
-          if nil == @telnet_session && nil != @telnet_hostip && nil != @telnet_username && nil != @telnet_password
-            telnet_session = Puppet::NetDev::CE::TelnetSession.new(@telnet_hostip, @telnet_username, @telnet_password)
-            @telnet_session = telnet_session.connect
+          if nil == @@telnet_session && nil != @@tel_instance.telnet_hostip && nil != @@tel_instance.telnet_username && nil != @@tel_instance.telnet_password
+           
+            telnet_session = Puppet::NetDev::CE::TelnetSession.new(@@tel_instance.telnet_hostip, @@tel_instance.telnet_username, @@tel_instance.telnet_password)
+            @@telnet_session = telnet_session.connect
             puts 'create telnet session in normal way'
-
-          # Below should be deleted, just for testing "puppet resource XXX" commands
-          elsif nil == @telnet_session && nil == @telnet_hostip && nil == @telnet_username && nil == @telnet_password
-            telnet_session_back = Puppet::NetDev::CE::TelnetSession.new('10.136.13.30', 'huawei', 'Admin@123')
-            @telnet_session = telnet_session_back.connect
-            puts 'create telnet session not in normal way'
+            
           end
 
-          @telnet_session
+          @@telnet_session
         end
 
-        def self.set_telnet_ip(telnet_ipaddress)
-          @telnet_hostip = telnet_ipaddress
+        def self.tel_instance(telnet_ipaddress, telnet_username, telnet_password)
+          @@tel_instance ||= new(telnet_ipaddress, telnet_username, telnet_password)
         end
 
-        def self.set_telnet_username(telnet_username)
+        def initialize(telnet_ipaddress, telnet_username, telnet_password)
+          @telnet_hostip   = telnet_ipaddress
           @telnet_username = telnet_username
-        end
-
-        def self.set_telnet_password(telnet_password)
           @telnet_password = telnet_password
         end
 
         # create Command_telnetApi instance
         def self.command_telnet_api
-          if nil == @command_telnet_api
-            @command_telnet_api = Command_telnetApi.new
+          if nil == @@command_telnet_api
+            @@command_telnet_api = Command_telnetApi.new
           end
-          @command_telnet_api
+          @@command_telnet_api
         end
       end
     end
