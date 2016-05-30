845077fdfc38719974fd458e2a39e0ba3a6311d7
makyomao <maoyingxia@huawei.com>
2016-05-30 16:22:17 +0800

    Add description of LICENSE and Modify

--- lib/puppet/provider/network_device/ce.rb
+++ lib/puppet/provider/network_device/ce.rb
@@ -1,3 +1,15 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # encoding: utf-8
 require 'puppet/provider/ce/device/device.rb'
 require 'puppet/provider/ce/api/apibase.rb'
@@ -13,8 +25,6 @@ Puppet::Type.type(:network_device).provide(:ce, parent: Puppet::Provider::CE) do
   end
 
   def flush
-    Puppet::NetDev::CE::Device.set_netconf_ip(resource[:ipaddress])
-    Puppet::NetDev::CE::Device.set_netconf_username(resource[:username])
-    Puppet::NetDev::CE::Device.set_netconf_password(resource[:password])
+    Puppet::NetDev::CE::Device.netconf_instance(resource[:ipaddress], resource[:username], resource[:password])
   end
 end
