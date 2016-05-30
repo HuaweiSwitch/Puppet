34e652effa81788e9be05bd728830393f2fac6d7
makyomao <maoyingxia@huawei.com>
2016-05-30 16:26:05 +0800

    Add description of LICENSE and Modify

--- lib/puppet/provider/network_device_tel/ce.rb
+++ lib/puppet/provider/network_device_tel/ce.rb
@@ -1,3 +1,15 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # encoding: utf-8
 require 'puppet/provider/ce/device_tel/device_tel.rb'
 require 'puppet/provider/ce/api/apibase.rb'
@@ -13,8 +25,6 @@ Puppet::Type.type(:network_device_tel).provide(:ce, parent: Puppet::Provider::CE
   end
 
   def flush
-    Puppet::NetDev::CE::Device.set_telnet_ip(resource[:telip])
-    Puppet::NetDev::CE::Device.set_telnet_username(resource[:teluser])
-    Puppet::NetDev::CE::Device.set_telnet_password(resource[:telpass])
+    Puppet::NetDev::CE::Device_tel.telnet_instance(resource[:telip], resource[:teluser], resource[:telpass])
   end
 end
