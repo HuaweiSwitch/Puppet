fcd0a70e612c7aea58dd57b3f94d9c9b910326e5
makyomao <maoyingxia@huawei.com>
2016-06-03 11:35:37 +0800

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,14 +11,5 @@
 # limitations under the License.
 
 class netdev_stdlib_ce{
-
-network_device{'CE-5850':
-name     => 'CE-5850',
-ipaddress => '10.136.68.123',
-username => 'rootDC',
-password => 'root_DC123',
+  include netdev_stdlib_ce::install
 }
-
-}
-
-
