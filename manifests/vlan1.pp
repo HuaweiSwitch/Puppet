ac60cfe5279fb5111b19be619fabc424c3e6e042
makyomao <maoyingxia@huawei.com>
2016-06-03 08:54:22 +0800

    Update vlan1.pp
    change the namespace to match in manifest

--- manifests/vlan1.pp
+++ manifests/vlan1.pp
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class puppetlabs_netdev_stdlib_ce::vlan1{
+class netdev_stdlib_ce::vlan1{
 
 network_vlan{'200':
 ensure     => present,
