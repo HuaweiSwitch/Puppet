07f86d2d63d30da183911a56c73496d52e08fc9b
makyomao <maoyingxia@huawei.com>
2016-06-03 08:53:20 +0800

    Update trunk2.pp
    change the namespace to mathc in manifest

--- manifests/trunk2.pp
+++ manifests/trunk2.pp
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class puppetlabs_netdev_stdlib_ce::trunk2{
+class netdev_stdlib_ce::trunk2{
 
 
 network_trunk{'10GE1/0/10':
